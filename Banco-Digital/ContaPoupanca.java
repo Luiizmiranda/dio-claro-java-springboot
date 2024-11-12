9fff67184983995655ec4e1bff46b7d89557dd74
Luiz Miranda <luizmiranda564@gmail.com>
2024-11-12 19:42:50 -0400

    Desafio
    Desafio

---
+++ Banco-Digital/ContaPoupanca.java
@@ -0,0 +1,11 @@
+
+public class ContaPoupanca extends Conta {
+    protected ContaPoupanca(String nomeCliente, Banco banco) {
+        super(nomeCliente, banco);
+    }
+
+    public void imprimirExtrato(){
+        System.out.println("=== Extrato Conta Poupança - Banco " + banco.getNome() + " ===");
+        imprimirDadosBancarios();
+    }
+}
---
+++ DesafioPOO/Main.java
@@ -0,0 +1,7 @@
+package DesafioPOO;
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        System.out.println(curso);
+    }
+}
